081404343ab8a8caec026522e48a45579cd4327a
silvester1215 <silvester1215@yahoo.com.tw>
2025-08-13 14:44:28 +0800

    Update photoscroll.js

--- scripts/photoscroll.js
+++ scripts/photoscroll.js
@@ -1,52 +1,50 @@
-const container = document.querySelector('.guide-container');
-if (container) {
-  container.innerHTML += container.innerHTML;
+if (window.innerWidth > 768) {
+  const container = document.querySelector('.guide-container');
+  if (container) {
+    container.innerHTML += container.innerHTML;
+    let scrollLeft = container.scrollLeft;
+    let isUserScrolling = false;
+    let isDraggingScrollbar = true;
+    let userScrollTimeout;
 
-  let scrollLeft = container.scrollLeft;
-  let isUserScrolling = false;
-  let isDraggingScrollbar = true;
-  let userScrollTimeout;
+    container.addEventListener('scroll', () => {
+      if (!isDraggingScrollbar) {
+        pauseAutoScrollWithDelay();
+      }
+    });
 
+    container.addEventListener('mousedown', (e) => {
+      if (e.offsetY > container.clientHeight) {
+        isDraggingScrollbar = true;
+        isUserScrolling = true;
+      }
+    });
 
-  // scroll 也暫停
-  container.addEventListener('scroll', () => {
-    if (!isDraggingScrollbar) {
-      pauseAutoScrollWithDelay();
-    }
-  });
+    document.addEventListener('mouseup', () => {
+      if (isDraggingScrollbar) {
+        isDraggingScrollbar = true;
+        scrollLeft = container.scrollLeft;
+        isUserScrolling = false;
+      }
+    });
 
-  // 拖曳橫向拉桿
-  container.addEventListener('mousedown', (e) => {
-    if (e.offsetY > container.clientHeight) {
-      isDraggingScrollbar = true;
+    function pauseAutoScrollWithDelay() {
       isUserScrolling = true;
+      clearTimeout(userScrollTimeout);
+      userScrollTimeout = setTimeout(() => {
+        isUserScrolling = false;
+      }, 1000);
     }
-  });
 
-  document.addEventListener('mouseup', () => {
-    if (isDraggingScrollbar) {
-      isDraggingScrollbar = true;
-      scrollLeft = container.scrollLeft; // ✅ 重新取得目前位置
-      isUserScrolling = false; // 立即恢復
+    function autoScroll() {
+      if (isUserScrolling) return;
+      scrollLeft += 1;
+      container.scrollLeft = scrollLeft;
+      if (scrollLeft >= container.scrollWidth / 2) {
+        scrollLeft = container.scrollLeft - (container.scrollWidth / 2);
+      }
     }
-  });
 
-  function pauseAutoScrollWithDelay() {
-    isUserScrolling = true;
-    clearTimeout(userScrollTimeout);
-    userScrollTimeout = setTimeout(() => {
-      isUserScrolling = false;
-    }, 1000);
+    setInterval(autoScroll, 30);
   }
-
-  function autoScroll() {
-    if (isUserScrolling) return;
-    scrollLeft += 1;
-    container.scrollLeft = scrollLeft;
-    if (scrollLeft >= container.scrollWidth / 2) {
-      scrollLeft = container.scrollLeft-(container.scrollWidth / 2) ;
-    }
-  }
-
-  setInterval(autoScroll, 30);
 }
